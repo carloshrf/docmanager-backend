2d107bf01fcb51df4c48ef3b0f74068c89bbe4c1
Carlos Henrique <carlos_6282010@hotmail.com>
2020-08-13 17:19:22 -0300

    Fixing return in the appointments route

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -32,7 +32,7 @@ appointmentsRouter.post('/', (request, response) => {
 
     return response.json(appointment);
   } catch (err) {
-    return response.json(400).json({ error: err.message });
+    return response.status(400).json({ error: err.message });
   }
 });
 
