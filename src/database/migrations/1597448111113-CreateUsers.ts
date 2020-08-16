d0e38f129f0a6f1f6fcc525c62d7761fc27a89cf
carloshrf <carlos_6282010@hotmail.com>
2020-08-15 21:27:07 -0300

    Creating foreign key in appoitnments migration and model

--- src/database/migrations/1597448111113-CreateUsers.ts
+++ src/database/migrations/1597448111113-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1597448111113 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
