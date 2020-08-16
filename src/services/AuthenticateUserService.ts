84ed76d5c09beade3c575a9009af6f71608d2ae8
carloshrf <carlos_6282010@hotmail.com>
2020-08-16 09:41:08 -0300

    using jwt to create tokens for registered users

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign, verify } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthentitcateUserService {
@@ -28,8 +30,14 @@ class AuthentitcateUserService {
       throw new Error('Incorrect email/password combination');
     }
 
+    const token = sign({}, '1024e3601957c7c065bcf06b2b621c4f', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
