76e7d314be18565b79041e4589b8c0add5524e59
carloshrf <carlos_6282010@hotmail.com>
2020-10-08 20:22:48 -0300

    testing errors on user authentication

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -2,6 +2,7 @@ import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepo
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
 import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
 import CreateUserService from '@modules/users/services/CreateUserService';
+import AppError from '@shared/errors/AppError';
 
 describe('AuthenticateUser', () => {
   it('should be able to authenticate', async () => {
@@ -16,7 +17,7 @@ describe('AuthenticateUser', () => {
       fakeHashProvider,
     );
 
-    await createUser.execute({
+    const user = await createUser.execute({
       name: 'cj',
       email: 'cj@email.com',
       password: '12345678',
@@ -28,5 +29,50 @@ describe('AuthenticateUser', () => {
     });
 
     expect(response).toHaveProperty('token');
+    expect(response.user).toEqual(user);
+  });
+
+  it('should not be able to authenticate with non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    expect(
+      authenticateUser.execute({
+        email: 'cj@email.com',
+        password: '12345678',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to authenticate with wrong password', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    await createUser.execute({
+      name: 'cj',
+      email: 'cj@email.com',
+      password: '12345678',
+    });
+
+    expect(
+      authenticateUser.execute({
+        email: 'cj@email.com',
+        password: '123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
